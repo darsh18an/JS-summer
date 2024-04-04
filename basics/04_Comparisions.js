76ea010c4d6d92590c6b8a9e41a1b3c4cf0f5104
darsh18an <120701579+darsh18an@users.noreply.github.com>
2024-04-04 09:07:29 +0530

    Comparison

---
+++ basics/04_Comparisions.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// upto here it is all good 
+
+// different datatypes are compared here in typescript does'nt allow you to compare different datatypes
+console.log("2">1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
