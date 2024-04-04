5f79e5c6abade5e7e89dc24c36df977234e92f84
darsh18an <120701579+darsh18an@users.noreply.github.com>
2024-04-04 08:50:52 +0530

    Cinversion

---
+++ basics/03_Conversion_datatypes.js
@@ -0,0 +1,65 @@
+let score = "danny"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//  let valueInNumber = Number(score)
+//   console.log(typeof valueInNumber);
+//   console.log(valueInNumber); // o/p =>NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false =>
+ let isLoggedIn = "simphony";
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//   console.log(booleanIsLoggedIn);
+
+// "" => false
+// 1 => true; 0 => false
+// "simphony" => true
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+//  console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+ let value = 3
+ let negValue = -value
+//  console.log(negValue); op =>-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+ let str1 = "hello"
+ let str2 = "jhony"
+
+let str3 = str1 + str2
+ console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2"); =>32
+
+// console.log( (3 + 4) * 5 % 3);
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter); op=>101
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
