1657b4fe51bc98f7a4e77fb00a3af1eda59373ab
darsh18an <120701579+darsh18an@users.noreply.github.com>
2024-04-07 16:04:54 +0530

    merging arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,41 @@
+const marvelHeros =["thor","hulk","Ironman","Captain America","spiderman"];
+const dcHeros = ["Batman","aquaman","superman","flash"];
+
+//  marvelHeros.push(dcHeros); 
+ // output[
+//   'thor',
+//   'hulk',
+//   'Ironman',
+//   'Captain America',
+//   'spiderman',
+//   [ 'Batman', 'aquaman', 'superman', 'flash' ]
+// ]
+
+// push creates array in array because push method takes whole array as one element 
+
+
+// console.log(marvelHeros);
+// console.log(marvelHeros[5][2]);//superman
+
+// concat
+//  const allHeros = marvelHeros.concat(dcHeros);// concat rerturns in new array so create new variable .
+//  console.log(typeof allHeros,allHeros);
+
+
+// spread creates each and every element as single element same as glass scattered
+// const allNewHeros =[...marvelHeros,...dcHeros];
+// console.log(typeof allNewHeros, allNewHeros);
+// output object [
+//   'thor',
+//   'hulk',
+//   'Ironman',
+//   'Captain America',
+//   'spiderman',
+//   'Batman',
+//   'aquaman',
+//   'superman',
+//   'flash'
+// ]
+
+// FLAT
+
