226f0960ec9004c6fe5e41a81e23033f536e3912
darsh18an <120701579+darsh18an@users.noreply.github.com>
2024-04-10 18:06:12 +0530

    initial commit

---
+++ 03_basics/01_Scopes.js
@@ -0,0 +1,24 @@
+// if(true){
+//   let a=10;
+//   const b=20;
+//   var c=30;
+// }
+
+//console.log(a);// a is not defined
+// console.log(b); // b is not defined
+// console.log(c);//30 
+// as c can be acess from outside of the scope also, 
+// which is not gud thing which create conflicts while working on other function or in any other file.
+
+// let a = 300;
+
+// if (true) {
+//   let a = 10;
+//   const b = 20;
+//   var c = 30;
+//   console.log("Inner: ", a); //10
+// }
+
+// console.log(a);
+
+
