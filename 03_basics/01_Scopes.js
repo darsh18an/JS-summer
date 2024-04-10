c7961cc289ca98e759a68e484382c8aa1e006ad8
darsh18an <120701579+darsh18an@users.noreply.github.com>
2024-04-10 20:26:38 +0530

    hoisting

--- 03_basics/01_Scopes.js
+++ 03_basics/01_Scopes.js
@@ -22,3 +22,44 @@
 // console.log(a);
 
 
+// function one() {
+//   const userName="darsh18an";
+
+//   function two() {
+//     const website="git";
+//     console.log(userName); // but you can access username has it come under global scope
+//   }
+  
+// console.log(website); // it can't access the function two 
+// it is outside the scope of the function
+//    two();
+// }
+// one();
+
+
+if (true){
+  const userName = "savera";
+  if(userName=="savera"){
+    const website= "yotube";
+    // console.log(userName," " + website);
+  }
+  // console.log(website);// 1 not defined
+}
+// console.log(userName);// 2 not defined
+
+// Hoisting where we can access the function with defination before intillization also
+// but not in case of the expression function you cannot access the variable before intilization.
+
+console.log(addOne(5));// 6
+function addOne(num1){
+  return num1+1;
+}
+
+
+console.log(addTwo(5)); // Cannot access 'addTwo' before initialization
+const addTwo = function(num1){
+  return num1+2;
+}
+// console.log(addTwo(5));//7
+
+
