0e252df4c9e116e88fa7cf5a7f986b0e59437043
darsh18an <120701579+darsh18an@users.noreply.github.com>
2024-04-12 16:25:22 +0530

    arrow

--- 03_basics/03_Arrows.js
+++ 03_basics/03_Arrows.js
@@ -1,14 +1,14 @@
 
-const user = {
-  userName:"darshan",
-  price :458,
+// const user = {
+//   userName:"darshan",
+//   price :458,
 
-  welcomeMessage: function(){ //Unexpected identifier 'welcomeMessage'
-    // console.log(`${this.userName}, welcome to this page`);
-    //  console.log(this);
-  } 
+//   welcomeMessage: function(){ //Unexpected identifier 'welcomeMessage'
+//      console.log(`${this.userName}, welcome to this page`);
+//       // console.log(this);
+//   } 
 
-}
+// }
 
 // user.welcomeMessage();
 // user.userName="sam bhadur";
@@ -25,3 +25,42 @@ const user = {
 // ======================== prequist for function completes ==================
 
 // fuctions does'nt support the this keyword because this keyword usually refers to the object
+
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// const chai =  () => {
+//     let username = "hitesh"
+//     console.log(this);
+// }
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+// const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+// console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
